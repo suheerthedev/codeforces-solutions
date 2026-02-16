452fb6a50e6ace1bda9696b9012589dba00f82a7
suheerthedev <khansuheer1@gmail.com>
2026-02-16 17:06:37 +0500

    Solved 69A - Young Physicist

---
+++ 69A - Young Physicist.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    int  x= 0, y=0, z=0, a=0,b=0,c=0;
+    cin>>n;
+ 
+    while(n--){
+        cin>>a>>b>>c;
+        x += a;
+        y += b;
+        z += c;
+    }
+ 
+    if(x == 0 && y ==0 && z == 0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
