3f90c3126bcd7aee3d801896870f3bb4f40e5026
suheerthedev <khansuheer1@gmail.com>
2026-02-17 20:53:09 +0500

    Improved 266B - Queue at the School

--- sol.cpp
+++ sol.cpp
@@ -1,15 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> calculateIndices(vector<int> vec, string s, int n){
-    for(int i = 0; i < n; i++){
-        if(s[i] == 'B'){
-            vec[i] = i;
-        }
-    }
-    return vec;
-}
-
 int main(){
     int n, t;
     cin>>n>>t;
@@ -17,23 +8,21 @@ int main(){
     string s;
     cin>>s;
 
-    vector<int> indices(n,-1);
-    
     while(t--){
-        indices = calculateIndices(indices, s,n);
-        for(int i = 0; i < n; i++){
-            if(indices[i] != -1){
-                if(s[i+1] == 'G'){
-
-                s[i] = 'G';
-                s[i+1] = 'B';
-                }
+        for(int i = n; i >= 0; i--){
+            if(s[i-1] == 'B' && s[i] == 'G'){
+                s[i-1] = 'G';
+                s[i] = 'B';
+                i -=1;
             }
         }
-        fill(indices.begin(), indices.end(), -1);
     }
 
     cout<<s<<endl;
 
     return 0;
 }
+
+ 
+
+    
